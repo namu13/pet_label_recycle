e2be6c182b4c1fcd377b83d7f32de97e145866e2
namu <namun1303@gmail.com>
2022-10-22 12:40:24 +0900

    main page style complete

--- pet-label-front/src/App.js
+++ pet-label-front/src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import Nav from "./components/nav";
 
 function App() {
+  const [point, setPoint] = useState(0);
   return (
     <>
       <Nav />
       <div className="buttons">
         <div className="camera-btn">QR code 찍기</div>
-        <div className="point-container"></div>
+        <div className="point-container">
+          <span className="title">내 포인트</span>
+          <span className="number">{point}</span>
+        </div>
       </div>
     </>
   );
