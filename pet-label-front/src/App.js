38417d147d125c362c313d2de718e5609116ad81
namu <namun1303@gmail.com>
2022-10-22 14:17:36 +0900

    send data to backend

--- pet-label-front/src/App.js
+++ pet-label-front/src/App.js
@@ -1,32 +1,36 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Html5Qrcode } from "html5-qrcode";
+import axios from "axios";
 
 import Nav from "./components/nav";
 
 function App() {
   const [point, setPoint] = useState(0);
   const cameraStart = () => {
-    // This method will trigger user permissions
     Html5Qrcode.getCameras()
       .then((devices) => {
-        /**
-         * devices would be an array of objects of type:
-         * { id: "id", label: "label" }
-         */
         if (devices && devices.length) {
           var cameraId = devices[0].id;
-          // .. use this to start scanning.
           const html5QrCode = new Html5Qrcode("camera-btn", "reader");
           html5QrCode
             .start(
               cameraId,
               {
-                fps: 10, // Optional, frame per seconds for qr code scanning
-                qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+                fps: 10,
+                qrbox: { width: 250, height: 250 },
               },
               (decodedText, decodedResult) => {
-                console.log(decodedText);
+                if (decodedText) {
+                  html5QrCode.stop().then((ignore) => {
+                    // QR Code scanning is stopped.
+                  });
+                  axios
+                    .post("/user", {
+                      qrData: decodedText,
+                    })
+                    .then(axios.get("/point").then((res) => setPoint(res)));
+                }
               },
               (errorMessage) => {
                 // parse error, ignore it.
